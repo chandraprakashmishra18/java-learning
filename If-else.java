e13882d9e5925bbdd99d5fade6248ab86e927ff7
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-14 02:15:29 +0530

    with 3 variable

--- If-else.java
+++ If-else.java
@@ -3,13 +3,16 @@ class IfElse
     public static void main(String[] args) {
         int x = 8;
         int y = 9;
+        int z = 10;
 
-        if (x<y)
+        if (y>x && y>z)
         {
             System.out.println(y);
             System.out.println("Thankyou");
         }
-        else
+        else if(x>y && x>z)
             System.out.println(x);
+        else 
+            System.out.println(z);
     }
 }
