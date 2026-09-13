f3c6edbe2d6c9eeaf70db357ae775d2b0ba19f0e
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-14 02:12:08 +0530

    if else statemen prbn

--- If-else.java
+++ If-else.java
@@ -1,1 +1,15 @@
 class IfElse
+{
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 9;
+
+        if (x<y)
+        {
+            System.out.println(y);
+            System.out.println("Thankyou");
+        }
+        else
+            System.out.println(x);
+    }
+}
