dad84721d5923cb962c36f31ebaa442e31cc634e
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-16 00:29:54 +0530

    dowhiledone

--- DoWhileLoop.java
+++ DoWhileLoop.java
@@ -1,5 +1,9 @@
 public class DoWhileLoop {
     public static void main(String[] args) {
-        
+        int i = 5;
+
+        do { 
+            System.out.println("Hello " + i);
+        } while (i<=4);
     }
 }
