dde91ebc778eb038dddda9fb6ab63b2c4d0e9ebc
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-16 00:44:55 +0530

    done

--- ForLoop.java
+++ ForLoop.java
@@ -1,6 +1,9 @@
 public class ForLoop {
     public static void main(String[] args) {
         int i = 1;
-        
+        for(;i<=5;){
+            System.out.println("Day" + i);
+            i++;
+        }
     }
 }
