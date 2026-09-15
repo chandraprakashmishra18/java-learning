244d517d31c18a93b8c89b68e44656b81bb36647
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-15 23:56:45 +0530

    code done

---
+++ Switch-Expression.java
@@ -0,0 +1,13 @@
+class SwitchExprssn{
+    public static void main(String[] args){
+        String day = "Sunday";
+        String result = "";
+
+        switch(day){
+            case "Saturday","Sunday" -> System.out.println("6am");
+            case "Monday" -> System.out.println("8am");
+            default -> System.out.println("7am");
+        }
+
+    }
+}
