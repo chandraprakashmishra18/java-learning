01eeecb5e023236cd26049aa685eaaf1ae2d9c85
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-18 02:13:15 +0530

    using array in java

--- ArrayinJava.java
+++ ArrayinJava.java
@@ -8,9 +8,21 @@ public class ArrayinJava {
         int k = 8;
 
         int num[] = {5,6,7};
+        num[1] = 6;
+
+        int nums[] = new int[4];
+        nums[0] = 4;
+        nums[1] = 5;
+        nums[2] = 6;
+        nums[3] = 8;
+
         System.out.println(i);
         System.out.println(j);
         System.out.println(k);
         System.out.println(Arrays.toString(num));
+        System.out.println(num[2]);
+        System.out.println(num[1]);
+        System.out.println(nums[3]);
+
     }
 }
