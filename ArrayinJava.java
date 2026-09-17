6b83deba96a59d9870676db74abc7a1d97a5af49
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-18 02:07:56 +0530

    string

--- ArrayinJava.java
+++ ArrayinJava.java
@@ -1,5 +1,16 @@
+
+import java.util.Arrays;
+
 public class ArrayinJava {
     public static void main(String[] args) {
-        
+        int i = 6;
+        int j = 7;
+        int k = 8;
+
+        int num[] = {5,6,7};
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(Arrays.toString(num));
     }
 }
