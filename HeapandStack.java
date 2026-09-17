a4a4fa79099018a26b1ae5c444fab3bde58f3078
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-18 01:51:35 +0530

    HeapandStack

--- HeapandStack.java
+++ HeapandStack.java
@@ -1,5 +1,23 @@
+class Computer
+{
+    public void playMusic()
+    {
+        System.out.println("Music Played");
+    }
+
+    public String getMeAPen(int cost){
+        if(cost>=10)
+            return "Pen";
+        else
+            return "Nothing";
+    }
+}
 public class HeapandStack {
     public static void main(String[] args) {
-        
+        Computer obj = new Computer();
+        obj.playMusic();
+        String str = obj.getMeAPen(0);
+        System.out.println(str);
+
     }
 }
