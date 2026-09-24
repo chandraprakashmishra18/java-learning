d35d146727770783b49912c4238210fceabf99d6
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-09-24 19:43:49 +0530

    jackedarray

--- JaggedArray.java
+++ JaggedArray.java
@@ -5,12 +5,18 @@ public class JaggedArray {
         nums[0] = new int[3];
         nums[1] = new int[4];
         nums[2] = new int[2];
+        // nums[3] = new int[5];
 
         for(int i=0;i<nums.length;i++)
         {
-            for(int j=0;j<4;j++)
+            for(int j=0;j<nums[i].length;j++)
             {
                 nums[i][j] = (int)(Math.random() * 10);
+            }
+        }
+        for(int n[] : nums){
+            for(int m: n){
+                System.out.println();
             }
         }
 
